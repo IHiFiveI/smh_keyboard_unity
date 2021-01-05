9db669478cfab2b5c2e0559fcfea60b6f6cd4b1b
MangriMen <mangrimen@gmail.com>
2021-01-05 14:27:01 +0600

    Added smooth head rotation

--- fpfvec/Assets/Scripts/Enemy/playerDetection.cs
+++ fpfvec/Assets/Scripts/Enemy/playerDetection.cs
@@ -7,29 +7,34 @@ public class playerDetection : MonoBehaviour
     [SerializeField] private Collider firstSphere;
     [SerializeField] private Collider secondSphere;
     [SerializeField] private Transform head;
-    private GameObject player;
+    private Transform player;
     public bool IsPlayerInVision { get; set; }
     public bool IsPlayerClose { get; set; }
-    private Transform defaultHeadPosition;
+    private Vector3 defaultHeadRotation;
 
-    void Start()
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        defaultHeadPosition = head.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform.Find("Player Camera");
+        defaultHeadRotation = head.transform.rotation.eulerAngles;
     }
 
     void Update()
     {
+        Debug.DrawLine(head.position, head.position * 10, Color.green, 0.5f);
         if (IsPlayerInVision)
         {
             Debug.Log("following");
-            Vector3 playerDirection = (transform.position - player.transform.position).normalized;
-            head.transform.rotation = Quaternion.LookRotation(playerDirection);
+
+            Vector3 playerDirection = (head.position - player.position).normalized;
+            playerDirection = new Vector3(playerDirection.x, 0f, playerDirection.z);
+            head.transform.localRotation = Quaternion.LookRotation(playerDirection);
+
+            Debug.DrawLine(player.position, player.position + playerDirection * 10, Color.red, 0.5f);
         }
         else
         {
+            head.transform.rotation = Quaternion.RotateTowards(head.transform.rotation, Quaternion.Euler(defaultHeadRotation), 1);
             Debug.Log("not following");
-            head.transform.rotation = defaultHeadPosition.rotation;
         }
     }
 }
--- fpfvec/Assets/Scripts/Player/mouseMovement.cs
+++ fpfvec/Assets/Scripts/Player/mouseMovement.cs
@@ -6,6 +6,7 @@ public class mouseMovement : MonoBehaviour
 {
     public float mouseSensitivity = 700f;
     public Transform playerBody;
+    public Transform playerHead;
     float xRotation;
 
     void Start()
@@ -23,5 +24,6 @@ public class mouseMovement : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+        playerHead.localRotation = Quaternion.Euler(0f, 0f, xRotation);
     }
 }
